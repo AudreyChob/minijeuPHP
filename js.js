349b29a5aecf1cea1ab3100a221a628d5ad3f687
AudreyChob <62380768+AudreyChob@users.noreply.github.com>
2020-06-12 10:23:37 +0200

    fonctionnel

--- js.js
+++ js.js
@@ -1,69 +1,107 @@
     window.addEventListener('load', () =>{
-        buttonInit();
+        genererNb();
     })
 
-    var buttonValide = document.querySelector('.buttonValide');
+
     var buttonJeu = document.querySelector('.buttonJeu');
     var buttonReset = document.querySelector('.buttonReset');
 
+///// Initiation du jeu /////////////
 
-    async function buttonInit(){
-        getScore()
-        buttonValide.addEventListener('click', () =>{
-            genererNb()
-            //getRandomInt(100)
-        })
 
-        buttonJeu.addEventListener('click', () => {
-            setScore()     
-        })
-    
-        buttonReset.addEventListener('click', () => {
-            resetScore({'score' : 0})
-        })
-    }
+buttonJeu.addEventListener('click', () => {
+    setEssai()     
+})
 
-    async function genererNb(){
-        let request = await fetch('http://localhost/mini-jeu/php/php.php?action=genererNb', {
-            method: 'POST',
-                headers: {
+buttonReset.addEventListener('click', () => {
+    genererNb()
+})
+
+
+        async function genererNb(){
+
+            document.getElementById('essai').value="";
+            document.getElementById('resultat').innerHTML = "";
+
+            let data = {
+                nbAleatoire: Math.floor((Math.random() * 100) + 1)
+            };
+            alert("ok")
+            console.log('data: ' +data.nbAleatoire);
+            let request = new Request('http://localhost/mini-jeu/php/php.php?action=genererNb', {
+                method: 'POST',
+                headers:{
                     'Content-Type': 'application/json'
                 },
-                //body: JSON.stringify(data)
-            }) 
-        getRandomInt(100)
-        let response = await fetch(request);
-        console.log('generer'.request)
+                body: JSON.stringify(data)
+            })
+            await fetch(request)
+            
+        }
+ 
+
+
+
+// ////// esai du joueur //////
+async function setEssai(){
+    let essai = document.getElementById('essai').value;
+    let nbAleatoire;
+
+    // requette qui récupére le nombre alétoire dans la bdd
+    let response = await fetch('http://localhost/mini-jeu/php/php.php?action=setEssai')
+    if(response.ok){
+        let data = await response.json();
+        nbAleatoire = (data.nbAleatoire);
+    }
+    //console.log(data.nbAleatoire)
+
+    // /// incrémenter le score 
+    let reponse = await fetch('http://localhost/mini-jeu/php/php.php?action=setScore');
+    if(reponse.ok){
+        let donnee = await reponse.json();
+        score = (donnee.score);
+    }
+   // console.log(score); 
+    
+    ////// tests des valeurs et les afficher //////
+    
+    if (essai < nbAleatoire) {
+        document.getElementById('resultat').innerHTML = "essai " + score +  ":" + essai + " Ton nombre est trop petit";
+    }
+    if (essai > nbAleatoire) {
+        document.getElementById('resultat').innerHTML =  "essai " + score + ":" + essai + " Ton numbre est trop grand";
+    }
+    if (essai == nbAleatoire) {
+        if(score>1){
+            alert("Tu as gagné en :" + score + " essais") ;
+        }
+        else {
+            alert("Tu as gagné en :" + score + " essai") ;
+        }
         
     }
+}
 
-    async function getScore(){
-        let response = await fetch('http://localhost/mini-jeu/php/php.php?action=getScore');
-        //console.log(response)
-        if(response.ok){
-            let data = await response.json();
-            console.log(data.score);
-        }
-    }
+// async function getScore(){
+//     let response = await fetch('http://localhost/mini-jeu/php/php.php?action=getScore');
+//     //console.log(response)
+//     if(response.ok){
+//         let data = await response.json();
+//         console.log(data.score);
+//     }
+// }
 
-    async function setScore(){
-        let request = await fetch('http://localhost/mini-jeu/php/php.php?action=setScore', {
-            
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            //body: JSON.stringify(data)
-        }) 
-        //console.log(data)
-        let response = await fetch(request);
-        let responseData = await response.json();
-        console.log('setScore '.response)
-    }
-
-
-    function getRandomInt(max) {
-        let nbAleatoire = Math.floor(Math.random() * Math.floor(max));
-        console.log(nbAleatoire);
-        return nbAleatoire;
-    }
+// async function setScore(){
+//     let request = await fetch('http://localhost/mini-jeu/php/php.php?action=setScore', {
+        
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         //body: JSON.stringify(data)
+//     }) 
+//     //console.log(data)
+//     let response = await fetch(request);
+//     let responseData = await response.json();
+//     console.log('setScore '.response)
+// }}
